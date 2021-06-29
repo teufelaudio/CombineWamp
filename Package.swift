75e997ac4968461598bdc8b0b8cd431a41ae2e3c
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-06-29 17:36:54 +0200

    Add SPM Product AllStatic

--- Package.swift
+++ Package.swift
@@ -6,11 +6,12 @@ let package = Package(
     name: "CombineWamp",
     platforms: [.iOS(.v13), .tvOS(.v13), .macOS(.v10_15), .watchOS(.v6)],
     products: [
-        .library(name: "CombineWamp", targets: ["CombineWamp"])
+        .library(name: "CombineWamp", targets: ["CombineWamp"]),
+        .library(name: "CombineWampAllStatic", targets: ["CombineWampAllStatic"])
     ],
     dependencies: [
         .package(url: "https://github.com/teufelaudio/CombineWebSocket.git", .upToNextMajor(from: "0.1.1")),
-        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .upToNextMajor(from: "0.1.5"))
+        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .upToNextMajor(from: "0.1.7"))
     ],
     targets: [
         .target(
@@ -18,6 +19,13 @@ let package = Package(
             dependencies: [
                 "CombineWebSocket",
                 .product(name: "FoundationExtensions", package: "FoundationExtensions")
+            ]
+        ),
+        .target(
+            name: "CombineWampAllStatic",
+            dependencies: [
+                "CombineWebSocket",
+                .product(name: "FoundationExtensionsStatic", package: "FoundationExtensions")
             ]
         ),
         .testTarget(name: "CombineWampTests", dependencies: ["CombineWamp"]),
