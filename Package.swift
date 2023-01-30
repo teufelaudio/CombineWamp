5a225b24b5d30f4ec1622d75a75c4fb1789afa4a
Oguz Yuksel <74555694+OguzYuuksel@users.noreply.github.com>
2023-01-30 11:10:34 +0100

    Update `FoundationExtensions` versioning. (#20)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/teufelaudio/CombineWebSocket.git", .upToNextMajor(from: "0.1.1")),
-        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .exact("0.1.20"))
+        .package(url: "https://github.com/teufelaudio/FoundationExtensions.git", .upToNextMajor(from: "0.2.0"))
     ],
     targets: [
         .target(
